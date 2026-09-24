cdcdf3925cfc183c44e15fb3258e9285d02c9f99
mutanganajoseph <josephmutangana65@gmail.com>
2026-09-24 21:01:06 +0200

    New files

--- auca/Sem-6/database development with plsql/inclass/task.sql
+++ auca/Sem-6/database development with plsql/inclass/task.sql
@@ -1,20 +1,21 @@
 emp_id number prompt 'Enter employee id';
 
-declare 
-
+declare
 v_id number:=&emp_id;
 v_fname varchar2(50);
 v_lname varchar2(50);
-v_salary number(50);
+v_salary number;
 
 begin
+
 select first_name, last_name, salary into v_fname,v_lname,v_salary
 from employees
-where employee_id=emp_id;
+where employee_id=v_id;
 dbms_output.put_line(v_fname);
 dbms_output.put_line(v_lname);
 dbms_output.put_line(v_salary);
 
+EXCEPTION
 when no_data_found then
 dbms_output.put_line('No employee found');
 end;
